19942b4801a1304c738f7d6ce49a2e8bed225153
dev-gma <contato.guimalexandre@gmail.com>
2024-05-24 15:17:09 -0300

    ExemploForArray

---
+++ ExemploForArray.java
@@ -0,0 +1,13 @@
+public class ExemploForArray{
+
+    public static void main(String[] args) {
+
+        //em arrays os indice dos elementos inicia em zero "0"
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+          }  
+    }
+}
+
