c62dd49eafb46286a3eaab46b16628513efe319c
dev-gma <contato.guimalexandre@gmail.com>
2024-05-31 10:43:43 -0300

    Candidatura

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,17 +1,21 @@
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+public class TiposVariaveis { 
+    public static void main(String[] args) {
+        // Definindo o salário mínimo
         double salarioMinimo = 2500;
 
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+        // Exemplos de tipos de dados numéricos
+        short numeroCurto = 1; // Variável do tipo short
+        int numeroNormal = numeroCurto; // Conversão implícita de short para int
+        short numeroCurto2 = (short) numeroNormal; // Conversão explícita de int para short
 
+        // Definindo e alterando valor de uma variável
         int numero = 5;
+        numero = 10; // Alterando o valor da variável
 
-        numero = 10;
-
+        // Imprimindo o valor da variável no console
         System.out.print(numero);
 
+        // Constante para o valor de PI
         final double VALOR_DE_PI = 3.14;
     }
 }
